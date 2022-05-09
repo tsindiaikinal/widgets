c4c0ca53d6751cfc79dc6abd438c84b1ff455289
Alekseii <1914.ac@gmail.com>
2022-05-09 21:21:06 +0300

    [js] add function printData and addEventListener

--- exchange_rates/index.js
+++ exchange_rates/index.js
@@ -1,15 +1,45 @@
 (function () {
-    let dateForAPI = new Date();
-    console.log(dateForAPI.getDate());
-    console.log(dateForAPI.getMonth());
-    console.log(dateForAPI.getFullYear());
-    const urlAPI =
-      'https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014';
-    fetch(urlAPI).then(function (response) { 
+  function setDateValue(date = null) {
+    let urlAPI = null;
+    if (date === null) {
+      urlAPI =
+        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    } else {
+      urlAPI = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${date}&json`;
+      // console.log(date);
+    }
+    fetch(urlAPI)
+      .then(function (response) {
         if (response.ok) {
-            return response.json();
+          return response.json();
         }
-     }).then(function (data) { 
-        console.log(data);
+      })
+      .then(function (response) {
+        printData(response);
       });
-    })();
+  }
+
+  /* Print data from the bank */
+  function printData(data) {
+    if (document.querySelector(".exchange-rates__body")) {
+      let template = "";
+      data.forEach((el) => {
+        template += `<tr>
+                        <td class="exchange-rates__body-elem">${el.txt}</td>
+                        <td class="exchange-rates__body-elem">${el.rate}</td>
+                        <td class="exchange-rates__body-elem">${el.cc}</td>
+                        <td class="exchange-rates__body-elem">${el.exchangedate}</td>
+                      </tr>`;
+      });
+      document.querySelector(".exchange-rates__body").innerHTML = template;
+    }
+  }
+
+  setDateValue();
+
+  /* Listener date from calendar */
+  document.getElementById("date_rate").addEventListener("change", function (e) {
+    let inputDateValue = e.target.value.replace(/-/g, "");
+    setDateValue(inputDateValue);
+  });
+})();
